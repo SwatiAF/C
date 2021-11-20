676311d3e9f1ff4848734c83a27a509df8498cce
Swati <83855603+SwatiAF@users.noreply.github.com>
2021-11-20 09:35:51 +0530

    Using Functions

---
+++ DST/postfix.c
@@ -0,0 +1,17 @@
+//POSTFIX EVALUATION
+
+#include<stdio.h>
+#define SIZE 20
+
+int stack[SIZE];
+int top=-1;
+
+void push(int x)
+{
+    stack[++top] = x;
+}
+
+int pop()
+{
+    return stack[top--];
+}
